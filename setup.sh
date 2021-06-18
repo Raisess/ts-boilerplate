a361e4f87a9a764d3ab8b36e34df9329c1551087
Raisess <raisechif@gmail.com>
2021-06-18 19:57:36 -0300

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-read -p "Are u sure about that [y/n]: " opt
+read -p "Are u sure about that? [y/n]: " opt
 
 if [ $opt = "y" ]; then
 	echo "Installing dev dependencies..."
@@ -10,6 +10,19 @@ if [ $opt = "y" ]; then
 	echo "Removing template git history..."
 	rm -rf ./.git
 	echo "Git history removed!"
+
+	echo "Initing a new git local repository..."
+	git init
+	git checkout -b main
+	git branch -d master
+
+	# add remote
+	read -p "You want to add a remote repository? [y/n]: " rem
+	if [ $rem = "y" ]; then
+		git remote add origin $rem
+		echo "Added remote repository connection"
+	fi
+	echo "Git started!"
 
 	echo "Deleting setup file..."
 	rm ./setup.sh
